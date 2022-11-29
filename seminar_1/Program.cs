0f9d9a4b1103949501f824d51148972ef3800048
Ramazan <coldalbard.@gmail.com>
2022-11-29 08:14:01 +0300

    added new file

---
+++ seminar_1/Program.cs
@@ -0,0 +1,91 @@
+﻿//Console.Write("How old are you ?: ");
+//int test = Convert.ToInt32(Console.ReadLine());
+//Console.Write("After 6 years you will be a " + (test + 6) + " years old");
+
+
+//Console.Write("Enter your number : ");
+//int numb = Convert.ToInt32(Console.ReadLine());
+//int numbq = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine($"square of number {numb} is {numb*numb}");
+//int numb1 = 0;
+//numb1 = numb * numb;
+//if(numb1 == numbq)
+//{
+    //Console.WriteLine("True");
+//}
+//else
+//{
+    //Console.WriteLine("False");
+//}
+
+
+
+//Console.Write("Enter your number : ");
+//int numb3 = Convert.ToInt32(Console.ReadLine());
+//if(numb3 < 1 || numb3 > 7)
+//{
+    //Console.WriteLine("Error");
+//}
+//else if(numb3 == 1)
+//{
+    //Console.WriteLine("Понедельник");
+//}
+//else if(numb3 == 2)
+//{
+    //Console.WriteLine("Вторник");
+//}
+//else if(numb3 == 3)
+//{
+    //Console.WriteLine("Среда");
+//}
+//else if(numb3 == 4)
+//{
+    //Console.WriteLine("Четверг");
+//}
+//else if(numb3 == 5)
+//{
+   //Console.WriteLine("Пятница");
+//}
+//else if(numb3 == 6)
+//{
+    //Console.WriteLine("Суббота");
+//}
+//else if (numb3 == 7)
+//{
+    //Console.WriteLine("Воскресенье");
+//}
+
+
+// Console.Write("Enter your number : ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// int n1 = 0;
+// int n2 = 0;
+// n1 = n * (- 1);
+// if(n > 0)
+// {
+//     while(n1 <= n)
+//     {
+//         Console.WriteLine(n1 + " ");
+//         n1 = n1 + 1;
+//     }
+// }
+// if(n < 0)
+// {
+//     n2 = n * (-1);
+//     while(n <= n2);
+//     {
+//         Console.WriteLine(n + " ");
+//         n = n + 1;
+//     }
+// }
+
+
+
+// Console.Write("Enter your number : ");
+// int y = Convert.ToInt32(Console.ReadLine());
+// if(y < 100)
+// {
+//     Console.Write("error");
+// }
+// int y1 = (y % 10);
+// Console.Write(y1);
