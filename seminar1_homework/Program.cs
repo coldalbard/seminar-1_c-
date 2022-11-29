66e98784dbfbdb5f9fc7493838b89fecf62922c1
Ramazan <coldalbard.@gmail.com>
2022-11-29 09:04:45 +0300

    added homework

---
+++ seminar1_homework/Program.cs
@@ -0,0 +1,68 @@
+﻿//task 1
+// Console.Write("Enter your number: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Enter your number: ");
+// int b = Convert.ToInt32(Console.ReadLine());
+// int max = a;
+// if(max < a)
+// {
+//     max = a;
+// }
+// if(b > max)
+// {
+//     max = b;
+// }
+// Console.WriteLine("max = " + max);
+
+
+
+//task 2
+// Console.Write("Enter your number: ");
+// int a1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Enter your number: ");
+// int b1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Enter your number: ");
+// int c = Convert.ToInt32(Console.ReadLine());
+// int max1 = a1;
+// if(max1 < a1)
+// {
+//     max1 = a1;
+// }
+// if(b1 > max1)
+// {
+//     max1 = b1;
+// }
+// if(c > max1)
+// {
+//     max1 = c;
+// }
+// Console.WriteLine("max = " + max1);
+
+
+
+//task 3
+// Console.Write("Enter your number: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// if(n % 2 == 0)
+// {
+//     Console.WriteLine("Yes");
+// }
+// if (n % 2 != 0)
+// {
+//     Console.WriteLine("No");
+// }
+
+
+
+//task 4
+// Console.Write("Enter your number: ");
+// int numb = Convert.ToInt32(Console.ReadLine());
+// int count = 1;
+// while(count < numb + 1)
+// {
+//     if(count % 2 == 0)
+//     {
+//         Console.WriteLine(count);
+//     }
+//     count = count + 1;
+// }
