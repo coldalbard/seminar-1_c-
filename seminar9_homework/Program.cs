ba6ba555213e3bfb184fc45e3e2d41a718c56225
Ramazan <coldalbard.@gmail.com>
2022-12-26 00:38:27 +0300

    added new file

---
+++ seminar9_homework/Program.cs
@@ -0,0 +1,9 @@
+﻿// Задача 64: Написать рекурсивную функцию перевода 
+// из десятичной системы исчисления в двоичную.
+
+
+// int Binar(int number){
+
+// }
+// Console.Write("Please, enter your number: ");
+// int number = Convert.ToInt32(Console.ReadLine());
