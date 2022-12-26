4f66b97f45f84ac53c21100eebfa7b65d44c6ff7
Ramazan <coldalbard.@gmail.com>
2022-12-26 17:13:29 +0300

    +

--- seminar9_homework/Program.cs
+++ seminar9_homework/Program.cs
@@ -1,9 +1,24 @@
 ﻿// Задача 64: Написать рекурсивную функцию перевода 
 // из десятичной системы исчисления в двоичную.
 
-
-// int Binar(int number){
-
-// }
 // Console.Write("Please, enter your number: ");
 // int number = Convert.ToInt32(Console.ReadLine());
+
+
+// int[] BinarySystem(int number){
+//     int count = 0;
+//     int temp = number;
+//     while(temp != 0){
+//         temp /= 2;
+//         count++;
+//     }
+//     int[] array = new int [count];
+//     for(int i = 0; i < count; i++){
+//         array[count - 1 - i] = number % 2;
+//         number /= 2;
+//     }
+//     return array;
+// }
+
+// int[] res = BinarySystem(number);
+// Console.Write(res);
